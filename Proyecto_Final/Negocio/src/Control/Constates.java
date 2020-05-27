a7253dc26264358d20a209040d6d544e4755c873
andre <andre@DESKTOP-CROVT6T>
2020-05-27 17:01:23 -0500

    Arreglada La hora en interfaz

---
+++ Proyecto_Final/Negocio/src/Control/Constates.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+/**
+ *
+ * @author andre
+ */
+public class Constates {
+    private static final double VALOR_IMAGEN=0;
+    private static final double VALOR_VIDEO=0;
+    private static final String RUTA_ARCHIVO_LIBROS="a";
+    private static final String RUTA_REPORTE="a";
+
+    public static double getVALOR_IMAGEN() {
+        return VALOR_IMAGEN;
+    }
+
+    public static double getVALOR_VIDEO() {
+        return VALOR_VIDEO;
+    }
+
+    public static String getRUTA_ARCHIVO_LIBROS() {
+        return RUTA_ARCHIVO_LIBROS;
+    }
+
+    public static String getRUTA_REPORTE() {
+        return RUTA_REPORTE;
+    }
+
+    public Constates() {
+    }
+    
+    
+    
+}
