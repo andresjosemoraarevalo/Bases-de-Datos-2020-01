289634ca1e378f0155ade68d3dee3b37e8cdd245
andre <andre@DESKTOP-CROVT6T>
2020-05-28 02:19:34 -0500

    Oscar es gay

---
+++ Proyecto_Final/DTO/src/dto/DTOResumen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import entidades.Linea;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public class DTOResumen {
+        String mensaje;
+        List<DTOLinea> Lineas;
+        boolean agregar;
+        int totalPrestamo;
+        int saldoMonedas;
+        int cantidadVueltos;
+        
+        
+}
